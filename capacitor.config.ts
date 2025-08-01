0cdfffdb6ac87be46ed4b916fdf2d797ea44861b
Gustavo Passos <gusapas26@gmail.com>
2025-08-01 14:31:29 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mestra-ionic',
+  webDir: 'www'
+};
+
+export default config;
